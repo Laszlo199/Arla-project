4d9da82dc39c2325c8f3a2c795a58f31c5b9fb46
kubaczechowski <jcobczechowski@gmail.com>
2021-04-26 14:37:52 +0200

    set up and configurated project

---
+++ src/main/java/testContr.java
@@ -0,0 +1,11 @@
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class testContr implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("It works");
+    }
+}
