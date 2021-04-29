fb29865f605e93c7fbcd7567544073fc028e0803
kubaczechowski <jcobczechowski@gmail.com>
2021-04-29 16:41:18 +0200

    default template base fxml

--- src/main/java/GUI/Main.java
+++ src/main/java/GUI/Main.java
@@ -13,7 +13,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/main.fxml"));
+       // FXMLLoader loader = new FXMLLoader(getClass().getResource("/main.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Templates/CreateDefaultTemplate.fxml"));
+
         Parent root = loader.load();
         stage.setTitle("Production");
         Scene scene = new Scene(root);
