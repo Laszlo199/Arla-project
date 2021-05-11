d533ade463d091be3e590c2dcc0bb90ccf9d51ab
kubaczechowski <jcobczechowski@gmail.com>
2021-05-11 21:55:45 +0200

    set number of rows and cols

--- src/main/java/GUI/Controller/CreateNewScreenController.java
+++ src/main/java/GUI/Controller/CreateNewScreenController.java
@@ -1,11 +1,15 @@
 package GUI.Controller;
 
+import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextFormatter;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -15,6 +19,7 @@ import java.util.function.UnaryOperator;
  *
  */
 public class CreateNewScreenController implements Initializable {
+    @FXML private JFXButton setButton;
     @FXML private TextField rowsFiled;
     @FXML private TextField colsField;
     @FXML private AnchorPane space;
@@ -49,6 +54,27 @@ public class CreateNewScreenController implements Initializable {
      * @param actionEvent
      */
     public void setGrid(ActionEvent actionEvent) {
-
+        if(!colsField.getText().isEmpty() && !rowsFiled.getText().isEmpty()){
+            int cols = Integer.parseInt(colsField.getText());
+            int rows = Integer.parseInt(rowsFiled.getText());
+            GridPane gridPane = new GridPane();
+            gridPane.setGridLinesVisible(true);
+            gridPane.prefHeightProperty().bind(space.heightProperty());
+            gridPane.prefWidthProperty().bind(space.widthProperty());
+            for (int i = 0; i < cols; i++) {
+                ColumnConstraints colConst = new ColumnConstraints();
+                colConst.setPercentWidth(100.0 / cols);
+                gridPane.getColumnConstraints().add(colConst);
+            }
+            for (int i = 0; i < rows; i++) {
+                RowConstraints rowConst = new RowConstraints();
+                rowConst.setPercentHeight(100.0 / rows);
+                gridPane.getRowConstraints().add(rowConst);
+            }
+            space.getChildren().add(gridPane);
+            colsField.setDisable(true);
+            rowsFiled.setDisable(true);
+            setButton.setDisable(true);
+        }
     }
 }
