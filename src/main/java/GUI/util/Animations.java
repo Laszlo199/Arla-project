f2d919d735fe14236abd9735372f154844ef90f0
kubaczechowski <jcobczechowski@gmail.com>
2021-05-31 10:58:04 +0200

    log in animations

--- src/main/java/GUI/util/Animations.java
+++ src/main/java/GUI/util/Animations.java
@@ -1,6 +1,7 @@
 package gui.util;
 
 import javafx.animation.FadeTransition;
+import javafx.animation.TranslateTransition;
 import javafx.scene.Node;
 import javafx.util.Duration;
 
@@ -13,5 +14,20 @@ public class Animations {
         ft.setFromValue(0.4);
         ft.setToValue(1.0);
         ft.play();
+    }
+
+    /**
+     * animation moves node only horizontally
+     * it can be used for example if the access was denied
+     * and in one of the fields there was provided wrong information
+     * @param node
+     */
+    public static void shakeNodeAnimation(Node node) {
+        TranslateTransition translateTransition = new TranslateTransition(Duration.millis(50), node);
+        translateTransition.setFromX(0f);
+        translateTransition.setByX(10f);
+        translateTransition.setCycleCount(2);
+        translateTransition.setAutoReverse(true);
+        translateTransition.playFromStart();
     }
 }
